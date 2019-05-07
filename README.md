9fb4c8a0eacfe1fea49b7d2616a37d79a8910525
Illya Hulko <illya_hulko@epam.com>
2019-05-07 18:15:23 +0300

    Changed script to run without sudo; edited readme

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 [![Badge](https://img.shields.io/badge/state-work%20in%20progress-yellowgreen.svg)]()
 
 
-## initial-install - script that installs defined programs and applies configurations
+# initial-install - script that installs defined programs and applies configurations
 
-List of installables:
+### List of installables:
+
   * git
   * python3 | pip3
   * docker
@@ -12,3 +13,22 @@ List of installables:
   * golang
   * packer
   * vim | vimrc
+
+
+###List of actions:
+
+  * installed
+  * help
+
+### Work in progress: 
+
+  * ***CRITICAL:***
+    * export PATH variable without exiting script  
+
+  * **actions:** 
+    * installed
+  
+  * **code:**
+    * handling errorrs
+    * trapping Ctrl-C
+    * options to determine version of each product
--- initial-script.sh
+++ initial-script.sh
@@ -21,71 +21,69 @@ usage () {
 
 install_git () {
     echo "=== Installing Git.."
-    apt-get update
-    apt-get install git-core -y
+    sudo apt-get update
+    sudo apt-get install git-core -y
     git --version \
         && echo "=== Git has been successfully installed" && sleep 1
 }
 
 install_pip3 () {
     echo "=== Installing Python3 and Pip3.."
-    apt-get install python3 -y
-    apt-get install pip3 -y
-
+    sudo apt-get install python3 -y
+    sudo apt-get install python3-pip -y
 }
 
 install_docker () {
     echo "=== Installing Docker.."
-    apt-get remove docker docker-engine docker.io containerd runc
-    apt-get update
-    apt-get install \
+    sudo apt-get update
+    sudo apt-get install \
         apt-transport-https \
         ca-certificates \
         curl \
         gnupg-agent \
         software-properties-common -y
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) \
-    stable"
-    apt-get update
-    apt-get install docker-ce docker-ce-cli containerd.io -y
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository \
+        "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+        $(lsb_release -cs) \
+        stable"
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io -y
     echo "=== Adding $USER to docker group"
-    usermod -aG docker $USER
-    docker --version
+    sudo usermod -aG docker $USER
+    sudo docker --version
     echo "=== Re-login to use Docker with your account"
 }
 
 install_ansible () {
     echo "=== Installing Ansible.."
-    apt-get install software-properties-common
-    apt-add-repository --yes --update ppa:ansible/ansible
-    apt-get install ansible -y
+    sudo apt-get install software-properties-common
+    sudo apt-add-repository --yes --update ppa:ansible/ansible
+    sudo apt-get install ansible -y
     ansible --version
 }
 
 install_terraform () {
     local TERRAFORM_VERSION="0.11.13"
-    echo "=== Installing Terraform.."
-    apt-get install unzip -y
-    wget -c https://releases.hashicorp.com/terraform/$TERRAFORM_VERSION/terraform_$TERRAFORM_VERSION_linux_amd64.zip
-    unzip terraform_$TERRAFORM_VERSION_linux_amd64.zip
-    rm -rf /usr/local/bin/terraform
-    mv terraform /usr/local/bin/
+    echo "=== Installing Terraform $TERRAFORM_VERSION.."
+    sudo apt-get install unzip -y
+    wget -c https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+    unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+    sudo rm -rf /usr/local/bin/terraform
+    sudo mv terraform /usr/local/bin/
     terraform --version
 }
 
 install_golang () {
     local GO_VERSION="1.12.5"
     local PROJECT_DIR="go"
-    echo "=== Installing Golang $GO_VERSION.."
+    echo "=== Installing Golang ${GO_VERSION}.."
     echo "!!! To complete golang installation you need to run this command: 'source sourcefile'"
-    apt-get update
-    wget -c https://dl.google.com/go/go$GO_VERSION.linux-amd64.tar.gz
-    tar -xvf go$GO_VERSION.linux-amd64.tar.gz
-    rm -rf /usr/local/go
-    mv -f go /usr/local/
+    sudo apt-get update
+    wget -c https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz
+    tar -xf go${GO_VERSION}.linux-amd64.tar.gz
+    sudo rm -rf /usr/local/go
+    sudo mv -f go /usr/local/
     GOROOT=/usr/local/go
     GOPATH=$HOME/$PROJECT_DIR
     PATH=$PATH:$GOPATH/bin:$GOROOT/bin
@@ -94,16 +92,18 @@ install_golang () {
     echo "export GOPATH=$HOME/$PROJECT_DIR">>sourcefile
     echo "export PATH=$PATH:$GOPATH/bin:$GOROOT/bin">>sourcefile
     echo "=== To complete golang installation you need to run this command: 'source sourcefile'"
-    # go version
 }
 
 install_packer () {
     go version > /dev/null 2>&1; [ $? != 0 ] \
-        && install_golang
+        && install_golang \
+        && echo "Run 'source sourcefile' and start installing again" \
+        && exit 0
     echo "=== Installing Packer.."
     mkdir -p $(go env GOPATH)/src/github.com/hashicorp && cd $_
     git clone https://github.com/hashicorp/packer.git
     cd packer
+    sudo apt-get install build-essential -y
     make dev
     packer --version
 }
@@ -111,21 +111,17 @@ install_packer () {
 install_vim () {
     local VIMRC_LOCATION="https://gist.githubusercontent.com/IlyaGulko/0c10b8d6cebb286daf4359b04439514e/raw/83b6651df7b7cbefda36938c217e70e3fd3b3588/vimrc"
     echo "=== Updating /etc/vim/vimrc"
-    apt-get install vim -y
-    wget -O /etc/vim/vimrc $VIMRC_LOCATION
+    sudo apt-get install vim -y
+    sudo wget -O /etc/vim/vimrc $VIMRC_LOCATION
 }
 
 installed () {
     echo "=== Checking what applications have been already installed"
 }
 
-[ $EUID != 0 ] \
-    && echo "=== Root privileges are required" \
-    && return 1
+# #################### MAIN ####################
 
-[ $# -eq 0 ] \
-    && usage \
-    && return 0
+[ $# -eq 0 ] && usage && exit 0
 
 for arg in "$@"; do
     case $arg in
