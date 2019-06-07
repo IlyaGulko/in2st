8c2e61b24be9e692bb1c0d90363fafff97a90b35
Ilya Gulko <iigulko99@gmail.com>
2019-06-07 12:10:35 +0300

    Update in2st.sh

--- in2st.sh
+++ in2st.sh
@@ -60,7 +60,7 @@ install_docker () {
 
 install_ansible () {
     echo "=== Installing Ansible.."
-    sudo apt-get install software-properties-common
+    sudo apt-get install software-properties-common -y
     sudo apt-add-repository --yes --update ppa:ansible/ansible
     sudo apt-get install ansible -y
     ansible --version \
